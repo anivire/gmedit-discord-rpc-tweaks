77f97d52d0d39b45a6dbfe3a4dd81fcacb932a71
Nikita Grichan <anivire.work@gmail.com>
2024-03-26 13:43:59 +0300

    feat: merged time with idle state and project

--- discordStatus/main.js
+++ discordStatus/main.js
@@ -4,33 +4,35 @@
             const DiscordRPC = require(PluginState.dir+"\\node_modules\\discord-rpc");
             const clientId = '831957674563993631';
             const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+            let startDate = Date.now();
             
             rpc.once('ready', () => {
                 setActivity();
             });
 
-            
-            
             const keys = [
                 ['GameMaker Project','GameMaker: Studio Project','GameMaker: Studio 2 Project'],
                 ['gm8','gms1','gms2'],
             ];
 
             var activity = {
-                details: "GMEdit",
-                state: "WelcomePage",
+                details: "Start page",
+                state: "Idling",
                 instance: false,
-                largeImageKey: 'icon512'
+                largeImageKey: "icon512",
+                startTimestamp: startDate
             }
+
             async function setActivity() {
                 if (!rpc) return;
                 rpc.setActivity(activity);
             }
+
             rpc.login({ clientId }).catch(console.error);
 
             GMEdit.on("projectOpen", function(e) {
                 activity.details = 'project: ' + e.project.displayName;
-                activity.startTimestamp = Date.now();
+                activity.startTimestamp = startDate;
                 let ver = $gmedit["gml.Project"].current.version.config.projectModeId;
                 if (ver != 2 && ver != 1) {
                     ver = 0;
@@ -41,17 +43,19 @@
             });
 
             GMEdit.on("projectClose", function(e) {
-                activity.details = 'GMEdit';
-                activity.state = 'WelcomePage';
-                activity.startTimestamp = null;
+                activity.details = "Start page";
+                activity.state = "Idling";
+                activity.startTimestamp = startDate;
                 activity.smallImageKey = 'null';
                 activity.smallImageText = 'null';
                 rpc.setActivity(activity);
             });
+
             GMEdit.on("fileOpen", function(e) {
                 activity.state = e.file.name;
                 rpc.setActivity(activity);
             });
+
             GMEdit.on("activeFileChange", function(e) {
                 activity.state =  e.file.name;
                 if (e.file.name != "WelcomePage") {
